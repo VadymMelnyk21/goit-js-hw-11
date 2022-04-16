f2f04a8e0f5d7be006d5a348fe04004b9fa6bbde
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-16 19:15:29 +0300

    Бібліотека SimpleLightbox

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/common.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
