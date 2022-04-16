041d9246e421685603b3c054e796287d25ede349
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-16 19:44:08 +0300

    підключення форми

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/common.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+const searchFormRef = document.querySelector('form');
+const galleryRef = document.querySelector('.gallery');
+
+let input;
+let page = 1;
+
+searchFormRef.addEventListener('submit', createQuery);
+
+function createQuery(e) {
+    e.preventDefault();
+    input = e.currentTarget.searchQuery.value.trim();
+    galleryRef.innerHTML = '';
+
+    // if (input) {
+    //     fetchImage(input, page);
+    // }
+    console.log(input);
+}
