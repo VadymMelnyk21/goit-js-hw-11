e5de1cc372139b094b9bc52fff5582d99b261e93
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-17 01:58:21 +0300

    запит

--- src/js/fetchImage.js
+++ src/js/fetchImage.js
@@ -4,6 +4,17 @@ const key = '26782517-d7749598e3fd4393206db4041';
 axios.defaults.baseURL = 'https://pixabay.com/api'
 
 export async function fetchImage(name, page) {
-    const { data } = await axios.get(`/?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
+    const { data } = await axios.get(`/`,
+        {
+            params: {
+                key: `${key}`,
+                q: `${name}`,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: 'true',
+                page: `${page}`,
+                per_page: '40',
+            }
+        });
     return data;
 }
