578df3b4b0fc274a77828c42f4269b147f7a5e4b
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-17 20:23:32 +0300

    стилі

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import './css/common.css';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
